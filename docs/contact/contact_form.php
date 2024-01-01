a3adb15eca9bd7227b2d539dbe2b4565e8e42b5a
Joey Trampush <j.trampush@gmail.com>
2023-12-31 17:11:11 -0800

    link color

--- docs/contact/contact_form.php
+++ docs/contact/contact_form.php
@@ -5,11 +5,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $message = $_POST['BigText'];
 
     // Save to file (append mode)
-    $file = fopen("../docs/contact/contact_submissions.txt", "a");
+    $file = fopen("/docs/contact/contact_submissions.txt", "a");
     fwrite($file, "First Name: $firstName, Last Name: $lastName, Email: $email, Message: $message\n");
     fclose($file);
 
     // Redirect or display a message
     echo "Thank you for your submission!";
 }
-<?php
